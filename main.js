e41d693c56195db004eb61affd160b605ab33376
Sontsevolosa <sontsevolosa@gmail.com>
2016-02-01 13:08:55 +0200

    Update main.js

--- main.js
+++ main.js
@@ -6,14 +6,8 @@ var purple = [280, 50, 60];
 
 var myName = "Masyaka";
 letterColors = [red, orange, green, blue, purple];
-if(10 > 3) {
-    bubbleShape = "circle";
-}
 
-else {
-    bubbleShape = "square";
-}
-
+bubbleShape = "circle";
 
 drawName(myName, letterColors);
 bounceBubbles();
